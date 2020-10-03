b4dfe9cd167f3120c6e5d355693b008b40e9e332
hami-k <hamidakhatri@gmail.com>
2020-10-02 23:12:47 -0500

    Updated

--- Flixx/ViewController.swift
+++ Flixx/ViewController.swift
@@ -22,8 +22,6 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         tableView.dataSource = self
         
-        print("Hello")
-        
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
